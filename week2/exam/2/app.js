c66be8bef79d15762d53f3dabf3504a7d2bc36c7
mimipaskova <maria.paskova3@gmail.com>
2014-06-15 15:41:32 +0300

    pochti vsichko e napraveno

--- week2/exam/2/app.js
+++ week2/exam/2/app.js
@@ -1,9 +1,10 @@
 "use strict";
 
 $(document).ready(function(){
-  var clock;
-  //alert("start");
+
+
   $("#up").on("click",function(){
+    var clock;
     var minutes = 0;
     var seconds = 0;
     var max_minutes = $("#minutes").val();
@@ -26,23 +27,34 @@ $(document).ready(function(){
       $("#second-first-digit").html(dec_seconds);
       $("#second-second-digit").html(edin_seconds);
 
-
-
-
     }, 10);
-    //setInterval(function(){
-      //alert("Hello")
-    //}, 3000);
   });
 
   $("#down").on("click",function(){
-    //var intervalID = window.setInterval(function(){
+    var clock;
+    var max_minutes = $("#minutes").val();
+    var max_seconds = $("#seconds").val();
+    clock = window.setInterval(function(){
+      max_seconds--;
+      if(max_seconds < 1){
+        max_minutes--;
+        max_seconds=60;
+      }
 
+      var dec_min=Math.floor(max_minutes/10);
+      var edin_min=max_minutes%10;
+      var dec_seconds=Math.floor(max_seconds/10);
+      var edin_seconds=max_seconds%10;
+      $("#minute-first-digit").html(dec_min);
+      $("#minute-second-digit").html(edin_min);
+      $("#second-first-digit").html(dec_seconds);
+      $("#second-second-digit").html(edin_seconds);
+      if(dec_min < 1 && edin_min < 1 && dec_seconds < 1 && edin_seconds < 2){
+        window.clearInterval(clock);
+        $("#second-second-digit").html(0);
+      }
 
-    //}, 500);
-    //setInterval(function(){
-      //alert("Hello")
-    //}, 3000);
+    }, 1000);
   });
 
   $(".btn-danger").on("click",function(){
