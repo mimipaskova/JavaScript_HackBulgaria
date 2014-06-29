014570823c90a3e2bb2289e5dd5de31916c76dd5
mimipaskova <maria.paskova3@gmail.com>
2014-06-30 00:20:59 +0300

    some style fixes and delete wndow from window.clearInterval() function

--- week2/exam/2/app.js
+++ week2/exam/2/app.js
@@ -6,24 +6,24 @@ $(document).ready(function(){
 
   $("#up").on("click",function(){
 
-    var minutes = 0;
-    var seconds = 0;
-    var max_minutes = $("#minutes").val();
-    var max_seconds = $("#seconds").val();
-    window.clearInterval(clock);
-    clock = window.setInterval(function(){
+    var minutes = 0,
+      seconds = 0,
+      max_minutes = $("#minutes").val(),
+      max_seconds = $("#seconds").val();
+    clearInterval(clock);
+    clock = setInterval(function(){
       seconds++;
       if(seconds > 59){
         minutes++;
         seconds=0;
       }
       if(minutes >= max_minutes && seconds >= max_seconds){
-        window.clearInterval(clock);
+        clearInterval(clock);
       }
-      var dec_min=Math.floor(minutes/10);
-      var edin_min=minutes%10;
-      var dec_seconds=Math.floor(seconds/10);
-      var edin_seconds=seconds%10;
+      var dec_min=Math.floor(minutes/10),
+        edin_min=minutes%10,
+        dec_seconds=Math.floor(seconds/10),
+        edin_seconds=seconds%10;
       $("#minute-first-digit").html(dec_min);
       $("#minute-second-digit").html(edin_min);
       $("#second-first-digit").html(dec_seconds);
@@ -33,26 +33,26 @@ $(document).ready(function(){
   });
 
   $("#down").on("click",function(){
-    var max_minutes = $("#minutes").val();
-    var max_seconds = $("#seconds").val();
-    window.clearInterval(clock);
-    clock = window.setInterval(function(){
+    var max_minutes = $("#minutes").val(),
+      max_seconds = $("#seconds").val();
+    clearInterval(clock);
+    clock = setInterval(function(){
       max_seconds--;
       if(max_seconds < 0){
         max_minutes--;
         max_seconds = 59;
       }
 
-      var dec_min=Math.floor(max_minutes/10);
-      var edin_min=max_minutes%10;
-      var dec_seconds=Math.floor(max_seconds/10);
-      var edin_seconds=max_seconds%10;
+      var dec_min=Math.floor(max_minutes/10),
+        edin_min=max_minutes%10,
+        dec_seconds=Math.floor(max_seconds/10),
+        edin_seconds=max_seconds%10;
       $("#minute-first-digit").html(dec_min);
       $("#minute-second-digit").html(edin_min);
       $("#second-first-digit").html(dec_seconds);
       $("#second-second-digit").html(edin_seconds);
       if(dec_min < 1 && edin_min < 1 && dec_seconds < 1 && edin_seconds < 1){
-        window.clearInterval(clock);
+        clearInterval(clock);
         $("#second-second-digit").html(0);
       }
 
@@ -64,7 +64,7 @@ $(document).ready(function(){
     $("#minute-second-digit").html(0);
     $("#second-first-digit").html(0);
     $("#second-second-digit").html(0);
-    window.clearInterval(clock);
+    clearInterval(clock);
   });
 
 });
